f7288045ecb2703f6473a7452faa6033a0823082
mblester <morganmurry@gmail.com>
2022-07-07 14:21:38 -0500

    users controller and routes working

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,17 +1,23 @@
+const res = require("express/lib/response")
 const mysql = require("mysql")
 const pool = require("../mysql/connection")
 
 
 const getEmployees = (req, res) => {  
-    res.send('getting employees')
-}
+    pool.query('SELECT * FROM users', (err, rows) => {
+      res.json(rows)
+    })
+  }
 
 const getEmployeesById = (req, res) => {
-  let id = req.params.id
-  let found = employees.find((element) => {
-    return element.id == id;
+  let id = req.params.id;
+  let sql = "SELECT * FROM ?? WHERE ?? = ?";
+  sql = mysql.format(sql, ["users", "id", id]);
+
+  pool.query(sql, (err, rows) => {
+    //if (err) return handleSQLError(res, err)
+    return res.json(rows);
   })
-  res.send('Get employee my ID!')
 }
 
 const getEmployeesByFirstName = (req, res) => {
@@ -22,4 +28,39 @@ const getEmployeesByFirstName = (req, res) => {
   res.send('Get employee by first name!')
 }
 
-module.exports = {getEmployees, getEmployeesById, getEmployeesByFirstName}
+const createEmployee = (req, res) => {
+  console.log(req.body);
+  const { first_name, last_name } = req.body;
+  let sql = `INSERT INTO ?? VALUES (null, ?, ?)`;
+
+  sql = mysql.format(sql, ["users", first_name, last_name]);
+
+  pool.query(sql, (err, rows) => {
+    return res.json(rows);
+  })
+}
+
+const updateEmployee = (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  let sql = "UPDATE ?? SET ? WHERE ?? = ?";
+  sql = mysql.format(sql, ["users", body, "id", id]);
+
+  pool.query(sql, (err, results) => {
+    return res.status(204).json();
+  })
+}
+
+const deleteEmployeeById = (req, res) => {
+  const { id } = req.params;
+  let sql = "DELETE FROM ?? WHERE ?? = ?;";
+  sql = mysql.format(sql, ["users", "id", id])
+
+  pool.query(sql, (err, results) => {
+    console.log(err)
+    console.log(results)
+    return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
+  })
+}
+
+module.exports = {getEmployees, getEmployeesById, getEmployeesByFirstName, createEmployee, updateEmployee, deleteEmployeeById}
--- routes/employee.js
+++ routes/employee.js
@@ -7,5 +7,12 @@ const controller = require("../controllers/employees")
   router.get('/users/:id', controller.getEmployeesById)
 
   router.get('/firstname/:first_name', controller.getEmployeesByFirstName)
+  
+  router.post('/users', controller.createEmployee)
+
+  router.put('/users/:id', controller.updateEmployee)
+
+  router.delete('/users/:id', controller.deleteEmployeeById)
 
 module.exports = router;
+
