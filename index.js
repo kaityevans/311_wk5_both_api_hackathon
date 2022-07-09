3217063031391d462d3d54390a2e2fb719a5f907
KHONGTUB <jkojeda12@gmail.com>
2022-07-09 01:40:14 -0500

    Finished Address router and controller

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const employees = require('./routes/employee');
+const address = require('./routes/employeeAddress')
 
 const port = process.env.PORT || 4001;
 
 app.use(express.json())
-app.use( employees)
+app.use(employees)
+
+app.use(address)
 
 app.get('/', (req, res) => {
     res.send('Welcome to our API!')
